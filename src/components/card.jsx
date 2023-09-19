eaf79db3f8139b385317cd8d6ecd3b4041d469c6
TerryKrom <terrykrom1@gmail.com>
2023-09-19 10:37:20 -0300

    header responsivity fixed, and pagination

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './card.css';
 import 'atropos/css';
 import Atropos from 'atropos/react'
+import { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 
 const Card = ({ game }) => {
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+    img.src = game.thumbnail;
+  }, [game.thumbnail]);
+
   return (
     <Atropos className="my-atropos card" activeOffset={40}
       shadowScale={1.25}>
       <div className="atropos-scale">
         <div className="atropos-rotate">
           <div className="atropos-inner">
+            {imageLoaded ? (
               <img src={game.thumbnail} data-atropos-offset="-5" shadowScale={1.25} alt='game Thumbnail'/>
-              <div className="card-text text-over" data-atropos-offset="0">
-                <a href={game.game_url} className="center" target='_blank'>Download</a>
-              </div>
+            ) : (
+              <SkeletonTheme height={200} width={300} />
+            )}
+            <div className="card-text text-over" data-atropos-offset="0">
+              <a href={game.game_url} className="center" target='_blank'>Download</a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,19 +7,19 @@ const Header = () => {
         <>
             <header className='nav-menu'>
                 <div className='menu-links'>
-                        <Link to="/">Home</Link>
-                        <Link to="/categories">Categories</Link>
-                        </div>
-                    <div className='logo'>
-                        <img src="/logo.png" alt="logo"/>
-                    </div>
-                    <div className="menu-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/categories">Categories</Link>
+                </div>
+                <div className='logo'>
+                    <img src="/logo.png" alt="logo" />
+                </div>
+                <div className="menu-links">
                     <Link to="/catalogue">Catalogue</Link>
-                        <Link to="/about">About</Link>
-                    </div>
+                    <Link to="/about">About</Link>
+                </div>
             </header>
         </>
     );
 }
- 
-export default Header
+
+export default Header;
