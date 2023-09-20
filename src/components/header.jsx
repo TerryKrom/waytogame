e65333016ffdf251c4860c54e29a974683e89fc4
TerryKrom <terrykrom1@gmail.com>
2023-09-19 22:14:19 -0300

    header style fixed and bottom line of menu links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import './header.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <>
-            <header className='nav-menu'>
-                <div className='menu-links'>
-                    <Link to="/">Home</Link>
-                    <Link to="/categories">Categories</Link>
-                </div>
-                <div className='logo'>
-                    <img src="/logo.png" alt="logo" />
-                </div>
-                <div className="menu-links">
-                    <Link to="/catalogue">Catalogue</Link>
-                    <Link to="/about">About</Link>
-                </div>
-            </header>
-        </>
-    );
-}
 
+    const location = useLocation();
+
+    return (
+        <header className='nav-menu'>
+          <div className='menu-links'>
+            <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+              Home
+            </Link>
+            <Link
+              to="/categories"
+              className={location.pathname === '/categories' ? 'active' : ''}
+            >
+              Categories
+            </Link>
+          </div>
+          <div className='logo'>
+            <img src="/logo.png" alt="logo" />
+          </div>
+          <div className="menu-links">
+            <Link
+              to="/catalogue"
+              className={location.pathname === '/catalogue' ? 'active' : ''}
+            >
+              Catalogue
+            </Link>
+            <Link
+              to="/about"
+              className={location.pathname === '/about' ? 'active' : ''}
+            >
+              About
+            </Link>
+          </div>
+        </header>
+      );
+    };
 export default Header;
