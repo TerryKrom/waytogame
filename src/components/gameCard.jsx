8fff53f6eeed682a7ab5f976190fe9490b3f4fb1
TerryKrom <terrykrom1@gmail.com>
2024-01-02 20:30:47 -0300

    style improved

--- src/components/gameCard.jsx
+++ src/components/gameCard.jsx
@@ -5,13 +5,19 @@ const GameCard = ({ game }) => {
     return (
         <div className="game-card">
             <div className="game-card-body">
-                <img src={game.thumbnail} alt=""/>
+                <img src={game.thumbnail} alt="" />
                 <div className="game-card-text">
                     <h2>{game.title}</h2>
-                    <p className='platform'>Platform: {game.platform}</p>
-                    <p>Genre: {game.genre}</p>
+                    <p className='platform'>
+                        <span>Platform:</span>
+                        <span>{game.platform}</span>
+                    </p>
+                    <p>
+                        <span>Genre:</span>
+                        <span>{game.genre}</span>
+                    </p>
                     <div className="center">
-                      <a href={game.game_url} className="center" target='_blank'>Download</a>
+                        <a href={game.game_url} className="center" target='_blank'>Download</a>
                     </div>
                 </div>
             </div>
