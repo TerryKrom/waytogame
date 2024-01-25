7b8a0a13d2b01e4b61163b3cee2cb30534cdc0bd
TerryKrom <terrykrom1@gmail.com>
2024-01-25 12:47:02 -0300

    loading added on catalogue page

--- src/components/miniCardRow.jsx
+++ src/components/miniCardRow.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MiniCard from './miniCard';
+import Loading from './global/loading';
 
 const MiniCardRow = ({ data }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000)
+    }, [])
+
     return (
-        <div className="mini-card-row">
-            {data.map((game, index) => (
-                <MiniCard game={game} key={index} />
-            ))}
-        </div>
+        <>
+            {isLoading ? <Loading /> : 
+            <div className="mini-card-row">
+                {data.map((game, index) => (
+                    <MiniCard game={game} key={index} />
+                ))}
+            </div>
+            }
+        </>
+
     );
 };
 
