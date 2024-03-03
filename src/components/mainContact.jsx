036836210ab41876ec0f2d6ddd33db1af97e0551
TerryKrom <terrykrom1@gmail.com>
2024-03-03 12:26:36 -0300

    contact style fixed

--- src/components/mainContact.jsx
+++ src/components/mainContact.jsx
@@ -7,7 +7,7 @@ const MainContact = () => {
     return (  
         <div className="contact-container">
             <EmailContainer/>
-            <div className="left-column">
+            <div className="right-column">
                 <img src="/logo.png" alt="way to game logo" />
                 <h2>Need some Help?</h2>
                 <p>Check our social media!</p>
